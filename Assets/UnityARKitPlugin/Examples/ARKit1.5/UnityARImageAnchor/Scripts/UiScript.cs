351733cef7e94cd2a51ca73dc42f98077283420d
Dominik Doerner <nik_doerner@web.de>
2018-03-25 00:00:55 +0100

    UI Scene Updates

--- Assets/UnityARKitPlugin/Examples/ARKit1.5/UnityARImageAnchor/Scripts/UiScript.cs
+++ Assets/UnityARKitPlugin/Examples/ARKit1.5/UnityARImageAnchor/Scripts/UiScript.cs
@@ -1,26 +1,42 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-//using DG.Tweening;
 
 public class UiScript : MonoBehaviour {
 
     private IDigiModule _currentModule = new SearchModule();
 
+    private float _lerpTime;
+    private float _currentLerpTime;
+    private Vector3 _formTopPosition;
+    private Vector3 _formBottomPosition;
+    private Vector3 _formStartPosition;
+    private Vector3 _formEndPosition;
 
-	// Use this for initialization
-	void Start ()
+    // Use this for initialization
+    void Start ()
 	{
 	    foreach (var renderer in GetComponent("ObjectStore").GetComponentsInChildren<Renderer>())
 	    {
 	        renderer.enabled = false;
 	    }
+	    _formBottomPosition = GetComponent("FormPanel").transform.position;
+	    _formTopPosition = _formBottomPosition + new Vector3(0, 375, 0);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+	    //increment timer once per frame
+	    _currentLerpTime += Time.deltaTime;
+	    if (_currentLerpTime > _lerpTime)
+	    {
+	        _currentLerpTime = _lerpTime;
+	    }
+
+	    //lerp Form panel
+	    var perc = _currentLerpTime / _lerpTime;
+	    transform.position = Vector3.Lerp(_formStartPosition, _formEndPosition, perc);
+    }
 
     public void AdButton_OnClick(string sceneName)
     {
@@ -58,9 +74,10 @@ public class UiScript : MonoBehaviour {
         var formObject = GetComponent("FormPanel") as RectTransform;
         if (formObject == null)
             return;
-        
-		/*formObject.transform.DOMove(formObject.transform.position + new Vector3(0, 375, 0), 1f)
-			.SetEase(Ease.InOutElastic).SetDelay(0f);*/
+
+        _currentLerpTime = 0f;
+        _formStartPosition = _formBottomPosition;
+        _formEndPosition = _formTopPosition;
     }
 
     public void CloseForm()
@@ -68,8 +85,9 @@ public class UiScript : MonoBehaviour {
         var formObject = GetComponent("FormPanel") as RectTransform;
         if (formObject == null)
             return;
-
-        /*formObject.transform.DOMove(formObject.transform.position + new Vector3(0, -375, 0), 1f)
-			.SetEase(Ease.InOutElastic).SetDelay(0f);*/
+        
+        _currentLerpTime = 0f;
+        _formStartPosition = _formTopPosition;
+        _formEndPosition = _formBottomPosition;
     }
 }
