610d0a6149a9ad6c6e2c760ed92c65bb7c9a3d4e
r0ehre <usdkr@student.kit.edu>
2018-03-24 23:16:37 +0100

    added missing files

---
+++ Assets/RotationBehaviour.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotationBehaviour : MonoBehaviour {
+
+	private float speed = 70f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate(new Vector3(0, 1, 0), speed * Time.deltaTime, Space.World);
+	}
+}
