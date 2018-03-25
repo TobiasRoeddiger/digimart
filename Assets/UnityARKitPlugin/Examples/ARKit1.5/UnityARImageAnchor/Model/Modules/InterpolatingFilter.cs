92f4b233bdbb0e582d22baaa0e65f18b2dd97dff
Dominik Doerner <nik_doerner@web.de>
2018-03-25 09:39:29 +0200

    Improved color interpolation to bypass RGB interpolation problems

--- Assets/UnityARKitPlugin/Examples/ARKit1.5/UnityARImageAnchor/Model/Modules/InterpolatingFilter.cs
+++ Assets/UnityARKitPlugin/Examples/ARKit1.5/UnityARImageAnchor/Model/Modules/InterpolatingFilter.cs
@@ -8,6 +8,7 @@ public class InterpolatingFilter : IDigiFilter
     private double _max { get; set; }
 
     private static readonly Color MIN_COLOR = new Color(0.36f, 0.98f, 0.24f, 0.8f);
+    private static readonly Color MIDDLE_COLOR = new Color(1f, 0.93f, 0.19f, 0.8f);
     private static readonly Color MAX_COLOR = new Color(0.98f, 0.3f, 0.25f, 0.8f);
 
     public InterpolatingFilter(Func<Product, double> propertySelector, double min, double max)
@@ -27,16 +28,30 @@ public class InterpolatingFilter : IDigiFilter
         a = a > 1 ? 1 : a;
         
         var color = new Color(
-            InterpolateComponent(MIN_COLOR.r, MAX_COLOR.r, a),
-            InterpolateComponent(MIN_COLOR.g, MAX_COLOR.g, a),
-            InterpolateComponent(MIN_COLOR.b, MAX_COLOR.b, a),
-            InterpolateComponent(MIN_COLOR.a, MAX_COLOR.a, a)
+            InterpolateComponent(c => c.r, a),
+            InterpolateComponent(c => c.g, a),
+            InterpolateComponent(c => c.b, a),
+            InterpolateComponent(c => c.a, a)
         );
         return color;
     }
 
-    private static float InterpolateComponent(double a, double b, double lambda)
+    private static float InterpolateComponent(Func<Color, double> dim, double lambda)
     {
+        var a = dim(MIN_COLOR);
+        var b = dim(MAX_COLOR);
+
+        if (lambda < 0.5)
+        {
+            b = dim(MIDDLE_COLOR);
+            lambda = lambda * 2;
+        }
+        else
+        {
+            a = dim(MIDDLE_COLOR);
+            lambda = lambda * 2 - 1;
+        }
+
         return (float) (a + (b - a) * lambda);
     }
 }
